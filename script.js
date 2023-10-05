a6bf49101ee607a5d3c30457ed8262d8fe053353
Shivam Sharma <117676035+shivam123273@users.noreply.github.com>
2023-10-05 21:54:07 +0530

    Update script.js

--- script.js
+++ script.js
@@ -29,7 +29,6 @@ gsap.to("#nav", {
   scrollTrigger: {
     trigger: "#nav",
     scroller: "body",
-    // markers : true,
     start: "top -10%",
     end: "top -11%",
     scrub: 1,
@@ -41,7 +40,6 @@ gsap.to("#main", {
   scrollTrigger: {
     trigger: "#main",
     scroller: "body",
-    // markers: true,
     start: "top -25%",
     end: "top -70%",
     scrub: 2,
@@ -52,11 +50,9 @@ gsap.from("#about-us img, #about-us-in", {
   y: 50,
   opacity: 0,
   duration: 1,
-  // stagger: 0.4,
   scrollTrigger: {
     trigger: "#about-us",
     scroller: "body",
-    // markers: true,
     start: "top 70%",
     end: "top 65%",
     scrub: 1,
@@ -70,7 +66,6 @@ gsap.from(".card", {
   scrollTrigger: {
     trigger: ".card",
     scroller: "body",
-    // markers: true,
     start: "top 70%",
     end: "top 65%",
     scrub: 1,
@@ -83,7 +78,6 @@ gsap.from("#colon1", {
   scrollTrigger: {
     trigger: "#colon1",
     scroller: "body",
-    // markers: true,
     start: "top 55%",
     end: "top 45%",
     scrub: 3,
@@ -96,7 +90,6 @@ gsap.from("#colon2", {
   scrollTrigger: {
     trigger: "#colon1",
     scroller: "body",
-    // markers: true,
     start: "top 55%",
     end: "top 45%",
     scrub: 3,
@@ -108,7 +101,6 @@ gsap.from("#page4 h1", {
   scrollTrigger: {
     trigger: "#page4 h1",
     scroller: "body",
-    // markers: true,
     start: "top 95%",
     end: "top 85%",
     scrub: 3,
